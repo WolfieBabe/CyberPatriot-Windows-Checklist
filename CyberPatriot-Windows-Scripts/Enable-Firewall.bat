8abffb128d023c7e9a5e0db1d3f5d82de257b2d6
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2023-11-02 11:05:52 -0500

    Update Enable-Firewall.bat

--- CyberPatriot-Windows-Scripts/Enable-Firewall.bat
+++ CyberPatriot-Windows-Scripts/Enable-Firewall.bat
@@ -12,8 +12,14 @@ netsh advfirewall set logging allowedconnections enable
 netsh advfirewall set logging droppedconnections enable
 
 netsh advfirewall firewall set rule group="remote desktop" new enable=No
-netsh advfirewall firewall set rule name="Core Networking (DNS-Out)" dir=out action=allow
-netsh advfirewall firewall set rule name="Core Networking (DHCP-Out)" dir=out action=allow
+netsh advfirewall firewall set rule name="Core Networking - DNS (UDP-Out)" new dir=out action=allow
+netsh advfirewall firewall set rule name="Core Networking - Dynamic Host Configuration Protocol (DHCP-Out)" new dir=out action=allow
+netsh advfirewall firewall set rule name="Core Networking - IPv6 (IPv6-Out)" new dir=out action=block
+netsh advfirewall firewall set rule name="Core Networking - IPv6 (IPv6-Out)" new dir=in action=block
+netsh advfirewall firewall set rule name="Core Networking - Neighbor Discovery Advertisement (ICMPv6-Out)" new dir=out action=block
+netsh advfirewall firewall set rule name="Core Networking - Neighbor Discovery Advertisement (ICMPv6-Out)" new dir=in action=block
+netsh advfirewall firewall set rule name="Core Networking - Neighbor Discovery Solicitation (ICMPv6-Out)" new dir=out action=block
+netsh advfirewall firewall set rule name="Core Networking - Neighbor Discovery Solicitation (ICMPv6-Out)" new dir=in action=block
 
 netsh interface teredo set state disabled
 netsh interface ipv6 6to4 set state state=disabled undoonstop=disabled
