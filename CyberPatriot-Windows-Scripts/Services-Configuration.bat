82a234bf04f8ec5ee9734469d966afb900f00058
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2023-11-02 10:13:50 -0500

    Update Services-Configuration.bat

--- CyberPatriot-Windows-Scripts/Services-Configuration.bat
+++ CyberPatriot-Windows-Scripts/Services-Configuration.bat
@@ -35,4 +35,6 @@ sc stop WinRM
 sc config tlntsvr start= disabled
 sc stop tlntsvr
 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+
 pause
