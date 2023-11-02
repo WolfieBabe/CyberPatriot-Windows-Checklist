04b2ee1400af143a19af07ef7303d45af320467f
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2023-11-02 10:32:38 -0500

    Update Enable-Firewall.bat

--- CyberPatriot-Windows-Scripts/Enable-Firewall.bat
+++ CyberPatriot-Windows-Scripts/Enable-Firewall.bat
@@ -12,8 +12,7 @@ netsh advfirewall set logging allowedconnections enable
 netsh advfirewall set logging droppedconnections enable
 
 netsh advfirewall firewall set rule group="remote desktop" new enable=No
-netsh advfirewall firewall set rule name="Core Networking (DNS-Out)" dir=out action=allow protocol=UDP remoteport=53 program="c:\windows\system32\svchost.exe" service="dnscache"
-netsh advfirewall firewall add rule name="Core Networking (DHCP-Out)" dir=out action=allow protocol=UDP localport=68 remoteport=67 program="c:\windows\system32\svchost.exe" service="dhcp"netsh interface teredo set state disabled
-
+netsh advfirewall firewall set rule name="Core Networking (DNS-Out)" dir=out action=allow
+netsh advfirewall firewall set rule name="Core Networking (DHCP-Out)" dir=out action=allow
 
 pause
