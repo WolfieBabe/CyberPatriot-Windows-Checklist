e1699db5ca015ce6d54f531c38e2284f0e246ddd
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2023-11-03 10:21:39 -0500

    Update Miscellaneous.bat

--- CyberPatriot-Windows-Scripts/Miscellaneous.bat
+++ CyberPatriot-Windows-Scripts/Miscellaneous.bat
@@ -7,8 +7,3 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Real-Time Protec
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection" /v EnableNetworkProtection /t REG_DWORD /d 2 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Real-Time Protection" /v DpaDisabled /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Real-Time Protection" /v DpaDisabled /t REG_DWORD /d 0 /f
-
-reg set "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v CurrentLevel /t REG_DWORD /d 0x00011500 /f
-reg add "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v DpaDisabled /t REG_DWORD /d 0 /f
-reg add "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v DpaDisabled /t REG_DWORD /d 0 /f
-reg add "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v DpaDisabled /t REG_DWORD /d 0 /f
