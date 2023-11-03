971ca2cb982ca091701e64cf6b8274b31719146f
WolfieBabe <110204635+WolfieBabe@users.noreply.github.com>
2023-11-03 12:22:13 -0500

    Update Network-Config.bat

--- CyberPatriot-Windows-Scripts/Network-Config.bat
+++ CyberPatriot-Windows-Scripts/Network-Config.bat
@@ -1,5 +1,7 @@
 @echo off
 
+title Configure Network
+
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender" /v DisableAntiVirus /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Quarantine" /v PurgeItemsAfterDelay /t REG_DWORD /d 15 /f
